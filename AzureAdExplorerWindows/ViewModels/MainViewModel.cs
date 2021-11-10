b7b2ef3230a055c043408446d7a4c9a77f37c512
Marcus Carvalho <marcusca@microsoft.com>
2021-11-10 12:36:09 +0100

    Basic UI functionality

---
+++ AzureAdExplorerWindows/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureAdExplorerWindows.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        string loginText;
+        string resultText;
+
+        public IRelayCommand LoginCommand { get; }
+
+        public MainViewModel()
+        {
+            LoginText = "Login";
+
+            LoginCommand = new RelayCommand(OnLoginClicked);
+        }
+
+        public string LoginText
+        {
+            get => loginText;
+            set => SetProperty(ref loginText, value);
+        }
+
+        public string ResultText
+        {
+            get => resultText;
+            set => SetProperty(ref resultText, value);
+        }
+
+        private void OnLoginClicked()
+        {
+            ResultText = "Clicked";
+        }
+    }
+}
